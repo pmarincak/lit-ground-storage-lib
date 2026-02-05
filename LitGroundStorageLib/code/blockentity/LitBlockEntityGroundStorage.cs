6fd994062a68be1ace7805ae346131d1b271c82f
Paige Marincak <pmarincak@hotmail.com>
2026-02-04 21:27:31 -0500

    Get light value on load

--- LitGroundStorageLib/code/blockentity/LitBlockEntityGroundStorage.cs
+++ LitGroundStorageLib/code/blockentity/LitBlockEntityGroundStorage.cs
@@ -7,6 +7,16 @@ public class LitBlockEntityGroundStorage : BlockEntityGroundStorage
 {
     byte[] lightHSV = [0,0,0];
 
+    public override void Initialize(ICoreAPI api)
+    {
+        base.Initialize(api);
+
+        if (!inventory.Empty)
+        {
+            lightHSV = Block.GetLightHsv(Api.World.BlockAccessor, Pos);
+        }
+    }
+
     public override bool OnPlayerInteractStart(IPlayer player, BlockSelection bs)
     {
         var result = base.OnPlayerInteractStart(player, bs);
